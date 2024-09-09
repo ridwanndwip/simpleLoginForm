df3c120de2462932b56f36d1631024aab95d9aeb
ridwanndwip <ridwanndwip@gmail.com>
2024-09-09 20:45:33 +0700

    update image banner, detail icons and style css

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,20 @@ module.exports = {
   content: ["./*.{html,js}"],
   theme: {
     extend: {
+      dropShadow:{
+        '4xl' : '0px 20px 10px rgba(0, 0, 0, 0.30)',
+        // filter:  drop-shadow(0px 20px 10px rgba(0, 0, 0, $shadow-opacity));
+      },
+      fontFamily:{
+        'poppins' : ['Poppins', 'sans-serif'],
+      },
       colors:{
-        bgContainer: '#ffffff33',
+        colorContainer: '#ffffff33',
+        colorText: '#393E46',
+        colorBorder: '#86868b',
+      },
+      screens:{
+        'xs': '480px',
       },
     },
   },
