1babc511ac310ff8615897d8a6cd5197958a411c
ridwanndwip <ridwanndwip@gmail.com>
2024-09-16 00:21:59 +0700

    update background img and update transition

--- index.js
+++ index.js
@@ -3,12 +3,32 @@ const imgPass = document.querySelector('#form .imgPass');
 const userName = imgMail.nextElementSibling;
 const passWord = imgPass.nextElementSibling;
 
-userName.addEventListener('focus', () =>{
-  imgMail.style.opacity = '1';
+userName.addEventListener('focus',()=>{
+    imgMail.style.opacity = '1';
+    imgMail.parentElement.classList.add('active') 
 })
 
-userName.addEventListener('blur', () =>{
-  imgMail.style.opacity = '0';
+userName.addEventListener('blur',()=>{
+  if(userName.value.trim() !== ''){
+    imgMail.style.opacity = '1'; 
+  }else{
+    imgMail.style.opacity = '0'; 
+    imgMail.parentElement.classList.remove('active')
+  }
+})
+
+passWord.addEventListener('focus',()=>{
+  imgPass.style.opacity = '1';
+  imgPass.parentElement.classList.add('active') 
+})
+
+passWord.addEventListener('blur',()=>{
+if(passWord.value.trim() !== ''){
+  imgPass.style.opacity = '1'; 
+}else{
+  imgPass.style.opacity = '0'; 
+  imgPass.parentElement.classList.remove('active')
+}
 })
 
 const test = document.getElementById('signIn').addEventListener('click',function(){
