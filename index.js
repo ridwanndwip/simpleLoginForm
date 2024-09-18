17b1e88a8eff43488bc483ca074b929666ec61eb
ridwanndwip <ridwanndwip@gmail.com>
2024-09-18 16:14:43 +0700

    update banner and style

--- index.js
+++ index.js
@@ -1,42 +1,53 @@
-const imgMail = document.querySelector('#form .imgMail');
-const imgPass = document.querySelector('#form .imgPass');
+// const imgMail = document.querySelector('#form .imgMail');
+// const imgPass = document.querySelector('#form .imgPass');
 // const userName = imgMail.nextElementSibling;
 // const passWord = imgPass.nextElementSibling;
 
-imgMail.nextElementSibling.addEventListener('focus',()=>{
-    imgMail.style.opacity = '1';
-    imgMail.parentElement.classList.add('active')
-    imgMail.nextElementSibling.style.borderBottom = '1px solid #E7E7E7'
-})
-
-imgMail.nextElementSibling.addEventListener('blur',()=>{
-  if(imgMail.nextElementSibling.value.trim() !== ''){
-    imgMail.style.opacity = '1';
-    imgMail.nextElementSibling.style.borderBottom = '1px solid #E7E7E7'
-    
-  }else{
-    imgMail.style.opacity = '0'; 
-    imgMail.parentElement.classList.remove('active')
-    imgMail.nextElementSibling.style.borderBottom = '1px solid #6b7280'
-  }
-})
-
-imgPass.nextElementSibling.addEventListener('focus',()=>{
-  imgPass.style.opacity = '1';
-  imgPass.parentElement.classList.add('active') 
-  imgPass.nextElementSibling.style.borderBottom = '1px solid #E7E7E7'
-})
-
-imgPass.nextElementSibling.addEventListener('blur',()=>{
-if(imgPass.nextElementSibling.value.trim() !== ''){
-  imgPass.style.opacity = '1';
-  imgPass.nextElementSibling.style.borderBottom = '1px solid #E7E7E7' 
-}else{
-  imgPass.style.opacity = '0'; 
-  imgPass.parentElement.classList.remove('active')
-  imgPass.nextElementSibling.style.borderBottom = '1px solid #6b7280'
+const inputSignIn = document.getElementsByClassName('inputSignIn');
+for(let i = 0; i < inputSignIn.length; i++){
+  inputSignIn[i].addEventListener('focus', ()=> {
+    inputSignIn[i].previousElementSibling.style.opacity = '1';
+    inputSignIn[i].style.borderBottom = '1px solid #00B8A0';
+    inputSignIn[i].parentElement.classList.add('active');
+  })
 }
-})
+
+for(let i = 0; i < inputSignIn.length; i++){
+  inputSignIn[i].addEventListener('blur', ()=> {
+    if(inputSignIn[i].value.trim() !== ''){
+      inputSignIn[i].previousElementSibling.style.opacity = '1';
+      inputSignIn[i].style.borderBottom = '1px solid #E7E7E7'
+      
+    }else{
+      inputSignIn[i].previousElementSibling.style.opacity = '0'; 
+      inputSignIn[i].parentElement.classList.remove('active')
+      inputSignIn[i].style.borderBottom = '1px solid #6b7280'
+    }
+  })
+}
+
+// imgMail.nextElementSibling.addEventListener('blur',()=>{
+//   if(imgMail.nextElementSibling.value.trim() !== ''){
+//     imgMail.style.opacity = '1';
+//     imgMail.nextElementSibling.style.borderBottom = '1px solid #E7E7E7'
+    
+//   }else{
+//     imgMail.style.opacity = '0'; 
+//     imgMail.parentElement.classList.remove('active')
+//     imgMail.nextElementSibling.style.borderBottom = '1px solid #6b7280'
+//   }
+// })
+
+// imgPass.nextElementSibling.addEventListener('blur',()=>{
+// if(imgPass.nextElementSibling.value.trim() !== ''){
+//   imgPass.style.opacity = '1';
+//   imgPass.nextElementSibling.style.borderBottom = '1px solid #E7E7E7' 
+// }else{
+//   imgPass.style.opacity = '0'; 
+//   imgPass.parentElement.classList.remove('active')
+//   imgPass.nextElementSibling.style.borderBottom = '1px solid #6b7280'
+// }
+// })
 
 const test = document.getElementById('signIn').addEventListener('click',function(){
   if(userName.value == 'ridwan' && passWord.value == 'admin'){
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,21 +3,29 @@ module.exports = {
   content: ["./*.{html,js}"],
   theme: {
     extend: {
+      // backgroundImage: {
+      //   'custom-gradient': 'linear-gradient(to bottom, #3b82f6, #9333ea), url("img/banner.jpg")',
+      // },
       dropShadow:{
         '4xl' : '0px 20px 10px rgba(0, 0, 0, 0.30)',
         // filter:  drop-shadow(0px 20px 10px rgba(0, 0, 0, $shadow-opacity));
       },
       fontFamily:{
-        'poppins' : ['Poppins', 'sans-serif'],
+        'Montserrat' : ['Montserrat', 'sans-serif'],
       },
       colors:{
-        'baseColor' : '#E7E7E7'
+        'Color1' : '#00B8A0',
+        'Color1_90' : '#00B8A01A',
+        'Color1_20' : '#00B8A0CC',
+        'Color2' : '#E7E7E7',
+        'color2_30' : '#E7E7E74D',
       },
       screens:{
         'xs': '480px',
       },
       fontSize:{
         'xs': '10px',
+        'xs/2': '12px',
       }
     },
   },
